0580ed303b59684787d9e10b76d0d2b6db51f7da
“Sebastian <impactotenisok@gmail.com>
2023-11-22 10:31:03 -0300

    Se agregó componente catálogo que en realidad mustra mas info sobre la categoria celulares

--- src/Catalogo.jsx
+++ src/Catalogo.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { FetchCatalogo } from './FetchCatalo';
 
-let access_token =  localStorage.getItem('accessToken');
+let access_token = "APP_USR-7650143381075360-112122-482eebdc0434fdf7668dc554ca01c473-65494552";
+
 
 function Catalogo() {
-  const [datosCategoria, setDatosCategoria] = useState();
+  const [datosCategoria, setDatosCategoria] = useState({ results: [], available_filters: [], paging: {} });
 
   useEffect(() => {
     const fetchResultados = async () => {
-    //   const url = `https://api.mercadolibre.com/sites/MLA/search?category=MLA1051`;
-      const url = `https://api.mercadolibre.com/sites/MLA/search?category=MLA1055`
-      console.log(url);
+      const url = `https://api.mercadolibre.com/sites/MLA/search?category=MLA1055`;
 
       try {
         const response = await fetch(url, {
@@ -20,24 +20,112 @@ function Catalogo() {
         });
 
         const data = await response.json();
-        setDatosCategoria(data);
+        setDatosCategoria({
+          ...data,
+          loading: false,
+        });
+
       } catch (error) {
         console.error('Error al obtener información de la búsqueda:', error);
       }
     };
 
+
     // Llama a la función al montar el componente
     fetchResultados();
   }, []);
 
-
+  // Verificar si las propiedades existen antes de acceder a ellas
   console.log(datosCategoria);
 
   return (
-        <>
-        </>
+    <>
+      <div className='contenedorTablaPrincipal'>
+      <div className='categorias'>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <h2>{datosCategoria.filters?.[0]?.values?.[0]?.name}</h2>
+              </th>
+              <th><h2>{datosCategoria.paging?.total}</h2></th>
+            </tr>
+          </thead>
+          <tbody>
+              {datosCategoria.available_filters[0]?.values.map((datos) => (
+           <tr key={datos.index}>
+  <td  >{datos.name}</td>
+   <td>{datos.results}</td>
 
+              </tr>
+))}
+{datosCategoria.available_filters[2]?.values.map((datos) => (
+           <tr key={datos.index}>
+  <td  >{datos.name}</td>
+   <td>{datos.results}</td>
 
+              </tr>
+))}
+
+          </tbody>
+        </table>
+
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <h2>Localidad</h2>
+              </th>
+              <th><h2>Cantidad</h2></th>
+            </tr>
+          </thead>
+          <tbody>
+              {datosCategoria.available_filters[19]?.values.map((datos) => (
+           <tr key={datos.index}>
+  <td  >{datos.name}</td>
+   <td>{datos.results}</td>
+
+              </tr>
+))}
+{datosCategoria.available_filters[2]?.values.map((datos) => (
+           <tr key={datos.index}>
+  <td  >{datos.name}</td>
+   <td>{datos.results}</td>
+
+              </tr>
+))}
+
+          </tbody>
+        </table>
+
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <h2>Producto</h2>
+              </th>
+              <th><h3>Cantidad</h3></th>
+              <th><h3>Precio</h3></th>
+              <th><h3>Vendedor</h3></th>
+              <th><h3>Vendidas</h3></th>
+            </tr>
+          </thead>
+          <tbody>
+          {datosCategoria.results.map((datos) => (
+           <tr key={datos.index}>
+           <td  >{datos.title}</td>
+  <td  > {datos.available_quantity}</td>
+   <td> {datos.price}</td>
+   <td> {datos.seller.nickname}</td>
+   <td> {datos.sold_quantity}</td>
+              </tr>
+))}
+          </tbody>
+          </table>
+          </div>
+          
+      </div>
+    </>
   );
 }
 
---
+++ src/FetchCatalo.js
@@ -0,0 +1,42 @@
+//** Funciona pero el servidor te bloquea por tantas solicitudes */
+
+export const FetchCatalogo = async () => {
+    const limit = 50;
+    let offset = 0;
+    let allResults = [];
+    let loop=true
+  
+    try {
+      while (loop == true) {
+        const url = `https://api.mercadolibre.com/sites/MLA/search?category=MLA1055&offset=${offset}&limit=${limit}`;
+        const response = await fetch(url);
+  
+        if (!response.ok) {
+          throw new Error(`Error al realizar la solicitud: ${response.statusText}`);
+        }
+  
+        const data = await response.json();
+
+        console.log("datos de los 2100 prodctos" + data)
+  
+        if (data.results.length === 0 || offset >= data.paging.total) {
+            loop=false
+          // No hay más resultados o hemos alcanzado el total, salimos del bucle
+          break;
+        }
+  
+        // Agregamos los resultados actuales al array
+        allResults = allResults.concat(data.results);
+  
+        // Incrementamos el offset para la próxima solicitud
+        offset += limit;
+      }
+  
+      console.log('Todos los resultados:', allResults);
+    } catch (error) {
+      console.error('Error en la solicitud:', error.message);
+    }
+  };
+  
+  FetchCatalogo();
+  
